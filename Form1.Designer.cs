8d785e7382f8de8efc9ac7c3e7cfe819ee57d29c
Isolated <ismyskyllz@gmail.com>
2018-12-10 18:06:48 +0100

    Add Files

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(566, 254);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(566, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get DLL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 297);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "PS3Lib Checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Checker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static string FileName = null;
+        private string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+        public void GetDLL()
+        {
+            List<string> infecteddirectory = new List<string>();
+            using (IEnumerator<string> enumerator = GetFiles(path, "PS3Lib.dll", SearchOption.AllDirectories).GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    FileName = enumerator.Current;
+                    try
+                    {
+                        ModuleDefMD module = ModuleDefMD.Load(FileName);
+                        if (CheckLib(module))
+                        {
+                            richTextBox1.SelectionColor = Color.Red;
+                            richTextBox1.AppendText(FileName + Environment.NewLine);
+                            infecteddirectory.Add(FileName);                           
+                        }
+                        else
+                        {
+                            richTextBox1.SelectionColor = Color.Green;
+                            richTextBox1.AppendText(FileName + Environment.NewLine);
+                        }
+                    }
+                    catch { }
+                }
+            }
+            File.WriteAllLines(Application.StartupPath + @"\Infected.txt", infecteddirectory);
+            MessageBox.Show("L'application va redémarrer");
+            Application.Restart();
+        }
+
+        public static bool CheckLib(ModuleDefMD module)
+        {
+            foreach (TypeDef type in module.Types)
+            {
+                foreach (MethodDef method in type.Methods)
+                {
+                    if (type.Name.Equals("PS3API"))
+                    {
+                        if (method.Name.Equals("MakeInstanceAPI"))
+                        {
+                            if (method.HasBody == false)
+                            continue;
+                            if (method.Body.HasInstructions)
+                            {
+                                for (int i = 0; i < method.Body.Instructions.Count; i++)
+                                {
+                                    if (method.Body.Instructions[i].OpCode == OpCodes.Ldstr && method.Body.Instructions[i].Operand.ToString().Contains("TMAPI"))
+                                    {
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static IEnumerable<string> GetFiles(string root, string searchPattern, SearchOption p)
+        {
+            Stack<string> pending = new Stack<string>();
+            pending.Push(root);
+            while (pending.Count != 0)
+            {
+                string path = pending.Pop();
+                string[] next = null;
+                try
+                {
+                    next = Directory.GetFiles(path, searchPattern);
+                }
+                catch { }
+                if (next != null && next.Length != 0)
+                {
+                    foreach (string file in next)
+                    {
+                        yield
+                        return file;
+                    }
+                }
+                try
+                {
+                    next = Directory.GetDirectories(path);
+                    foreach (string item in next)
+                    {
+                        pending.Push(item);
+                    }
+                }
+                catch { }
+            }
+            yield
+            break;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetDLL();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(Application.StartupPath + @"\Infected.txt"))
+            {
+                string[] directory = File.ReadAllLines(Application.StartupPath + @"\Infected.txt");
+                int i = 0;
+                foreach (string line in directory)
+                {
+                    i++;
+                    File.Delete(line);
+                }
+                MessageBox.Show(i.ToString() + " Dll infectées ont été supprimées !", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                File.Delete(Application.StartupPath + @"\Infected.txt");
+            }
+        }
+    }
+}
